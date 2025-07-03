bf935fc32dfc4730be3feb14bc61f5db10b78b4d
SeaHogs <117960430+SeaHogs@users.noreply.github.com>
2025-07-03 10:05:32 +0800

    Bundle ML libs locally

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,4 @@
 (async function () {
-  // Dynamically load dependencies since content scripts aren't modules
-  async function loadScript(src) {
-    return new Promise((resolve, reject) => {
-      const s = document.createElement('script');
-      s.src = src;
-      s.onload = resolve;
-      s.onerror = reject;
-      document.head.appendChild(s);
-    });
-  }
-
-  // Load TensorFlow.js and Universal Sentence Encoder
-  await loadScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs');
-  await loadScript('https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder');
 
   // Dummy data for autofill
   const dummyData = {
